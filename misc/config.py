80a745f574cf85fb6c29db7d19c2ad1a9d95e58b
yterwe <goekengn@gmail.com>
2025-07-05 17:29:12 +0800

    Update config.py

--- misc/config.py
+++ misc/config.py
@@ -23,10 +23,10 @@ class Params:
             temp = params.get('issia_val_cameras', '5, 6')
             self.issia_val_cameras = [int(e) for e in temp.split(',')]
 
-        self.spd_path = params.get('spd_path', None)
-        if self.spd_path is not None:
-            temp = params.get('spd_set', '1, 2')
-            self.spd_set = [int(e) for e in temp.split(',')]
+        #self.spd_path = params.get('spd_path', None)
+        #if self.spd_path is not None:
+        #    temp = params.get('spd_set', '1, 2')
+        #    self.spd_set = [int(e) for e in temp.split(',')]
 
         self.num_workers = params.getint('num_workers', 0)
         self.batch_size = params.getint('batch_size', 4)
@@ -40,13 +40,13 @@ class Params:
 
     def _check_params(self):
         assert os.path.exists(self.issia_path), "Cannot access ISSIA CNR dataset: {}".format(self.issia_path)
-        assert os.path.exists(self.spd_path), "Cannot access SoccerPlayerDetection_bmvc17 dataset: {}".format(self.spd_path)
+        #assert os.path.exists(self.spd_path), "Cannot access SoccerPlayerDetection_bmvc17 dataset: {}".format(self.spd_path)
         for c in self.issia_train_cameras:
             assert 1 <= c <= 6, 'ISSIA CNR camera number must be between 1 and 6. Is: {}'.format(c)
         for c in self.issia_val_cameras:
             assert 1 <= c <= 6, 'ISSIA CNR camera number must be between 1 and 6. Is: {}'.format(c)
-        for c in self.spd_set:
-            assert c == 1 or c == 2, 'SPD dataset number must be 1 or 2. Is: {}'.format(c)
+        #for c in self.spd_set:
+        #    assert c == 1 or c == 2, 'SPD dataset number must be 1 or 2. Is: {}'.format(c)
 
     def print(self):
         print('Parameters:')
